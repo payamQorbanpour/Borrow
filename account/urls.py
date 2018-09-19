a8d095b35a4e41a202ea3809e355bec2e32ba0f8
payamQorbanpour <payamqorbanpour@gmail.com>
2018-09-19 16:43:08 +0430

    Added registeration system. Now user can edit her/is profile too.

--- account/urls.py
+++ account/urls.py
@@ -6,13 +6,17 @@ from .forms import LoginForm
 
 urlpatterns = [
 
+    # User login
     url(r'^login/$', auth_views.login, {'authentication_form':LoginForm}, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^logout-then-login/$', auth_views.logout_then_login, name='logout_then_login'),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
 
+    # User registration
+    url(r'^register/$', views.register, name='register'),
+
     # change password urls
-    url(r'^dashboard/password-change/$', auth_views.password_change, name='change_password'),
+    url(r'^dashboard/password-change/$', auth_views.password_change, name='password-change'),
     url(r'^dashboard/password-change/done/$', auth_views.password_change_done, name='password_change_done'),
 
     # restore password urls
@@ -20,4 +24,7 @@ urlpatterns = [
     url(r'^dashboard/password-reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^dashboard/password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^dashboard/password-reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
+
+    # User edit profile
+    url(r'^dashboard/edit/$', views.edit, name='edit'),
     ]
