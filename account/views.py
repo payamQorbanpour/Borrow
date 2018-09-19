628edb5d9c41cc134074081a83534f2c63ca8f6e
payamQorbanpour <payamqorbanpour@gmail.com>
2018-09-19 09:55:30 +0430

    A little modification in base.html for add things button and stuff.

--- account/views.py
+++ account/views.py
@@ -5,16 +5,19 @@ from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
+            user = authenticate(username=cd['username'],
+            password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated successfully')
+                    return HttpResponse('Authenticated '\
+                    'successfully')
                 else:
                     return HttpResponse('Disabled account')
             else:
