5423d9b88eaf26d3abb4ca71201960d1d468ec9a
payamQorbanpour <payamqorbanpour@gmail.com>
2018-09-20 12:29:59 +0430

    Now every authenricated user can add/create content.

---
+++ shop/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from django.contrib.auth.models import User
+from .models import Product
+
+
+class AddProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            "name",
+            "category",
+            "price",
+            "location",
+            "phone_number",
+            "health",
+        ]
