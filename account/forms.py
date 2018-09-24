6314f6ea5e75daaa077fbe1baf0c1a1d0e4d476e
payamQorbanpour <payamqorbanpour@gmail.com>
2018-09-24 22:01:28 +0330

    Modified register form style.

--- account/forms.py
+++ account/forms.py
@@ -19,12 +19,45 @@ class LoginForm(AuthenticationForm):
 
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
-
+    first_name = forms.CharField(widget=forms.TextInput(
+        attrs =
+        {
+        'class' : 'form-control',
+        }
+    ))
+    last_name = forms.CharField(widget=forms.TextInput(
+        attrs =
+        {
+        'class' : 'form-control',
+        }
+    ))
+    username = forms.CharField(widget=forms.TextInput(
+        attrs =
+        {
+            'class' : 'form-control',
+        }
+    ))
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs =
+        {
+        'class' : 'form-control',
+        }
+    ))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(
+        attrs =
+        {
+        'class' : 'form-control'
+        }
+    ))
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput(
+        attrs =
+        {
+        'class' : 'form-control'
+        }
+    ))
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email')
 
         def clean_password2(self):
             cd = self.cleaned_data
@@ -35,13 +68,36 @@ class UserRegistrationForm(forms.ModelForm):
 
 class UserEditForm(forms.ModelForm):
 
+    first_name = forms.CharField(widget=forms.TextInput(
+        attrs =
+        {
+        'class' : 'form-control',
+        }
+    ))
+    last_name = forms.CharField(widget=forms.TextInput(
+        attrs =
+        {
+        'class' : 'form-control',
+        }
+    ))
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs =
+        {
+        'class' : 'form-control col-lg-6',
+        }
+    ))
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
 
 
 class ProfileEditForm(forms.ModelForm):
-
+    photo = forms.ImageField(widget=forms.ClearableFileInput(
+        attrs =
+        {
+        'class' : 'form-control',
+        }
+    ))
     class Meta:
         model = Profile
         fields = ('photo',)
