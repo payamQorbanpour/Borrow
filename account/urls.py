82132a30345b88cfa412e58c6ba2a55eabcd051b
Payam <payamqorbanpour@gmail.com>
2019-07-26 12:16:39 +0430

    Modify basics

--- account/urls.py
+++ account/urls.py
@@ -8,8 +8,8 @@ from .forms import LoginForm
 urlpatterns = [
 
     # User login
-    url(r'^login/$', auth_views.login, {'authentication_form':LoginForm}, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^login/$', auth_views.LoginView.as_view(), {'authentication_form':LoginForm}, name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view()   , name='logout'),
     url(r'^logout-then-login/$', auth_views.logout_then_login, name='logout_then_login'),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
 
@@ -17,14 +17,14 @@ urlpatterns = [
     url(r'^register/$', views.register, name='register'),
 
     # change password urls
-    url(r'^dashboard/password-change/$', auth_views.password_change, name='password-change'),
-    url(r'^dashboard/password-change/done/$', auth_views.password_change_done, name='password_change_done'),
+    url(r'^dashboard/password-change/$', auth_views.PasswordChangeView.as_view(), name='password-change'),
+    url(r'^dashboard/password-change/done/$', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
     # restore password urls
-    url(r'^dashboard/password-reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^dashboard/password-reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    url(r'^dashboard/password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^dashboard/password-reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    url(r'^dashboard/password-reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    url(r'^dashboard/password-reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^dashboard/password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    url(r'^dashboard/password-reset/complete/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     # User edit profile
     url(r'^dashboard/edit/$', views.edit, name='edit'),
